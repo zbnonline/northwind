15af820a37755c1eac504e81515d802a5ecc3f46
anıl <nbr-nb@hotmail.com>
2023-07-26 12:17:29 +0300

    MyFinalProject front-end Angular krurulumu ve ilk component ile service yazılımı

---
+++ src/app/components/product/product.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { Product } from 'src/app/models/product';
+
+@Component({
+  selector: 'app-product',
+  templateUrl: './product.component.html',
+  styleUrls: ['./product.component.css']
+})
+export class ProductComponent {
+
+  products:Product[] = []
+
+  constructor(){ }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/models/category.ts
@@ -0,0 +1,4 @@
+export interface Category{
+  categoryId:Number;
+  categoryName:string;
+}
